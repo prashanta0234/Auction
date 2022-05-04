bbcb075ec55fc4ddfb3ddac11c4d57e8ed8fe28f
"Prashanta Chakraborty" <"prashanta0234@gmail.com">
2022-05-05 02:30:32 +0600

    bid

--- Auction.sol
+++ Auction.sol
@@ -4,12 +4,41 @@ pragma solidity >=0.7.0<0.9.0;
 contract Auction{
     address payable public manager;
     uint public endingTime;
-    uint public minimumBid;
+    uint public minimumBid;   
+    address public heightBider;
+    uint public heightBid;
+    address payable public owner;
 
-    constructor(uint EndingTime,uint MinumumBid){
+    struct Bidtype{
+        uint amount;
+        bool isBid;
+    }
+    mapping(address=>Bidtype) public bids;
+    constructor(uint EndingTime,uint MinumumBid,address _owner){
         manager=payable(msg.sender);
-        endingTime=EndingTime;
+        endingTime=block.timestamp+EndingTime;
         minimumBid=MinumumBid;
+        owner=payable(_owner);
 
     }
+    modifier isManger {
+        require(msg.sender==manager,"Sorry you are not the manager.");
+        _;
+    }
+    function bid() public payable {
+        require(msg.sender != manager,"Sorry manager not allowed for biding");
+        require(bids[msg.sender].isBid==true||minimumBid <= msg.value,"Please Send minimum value");
+        require(bids[msg.sender].amount+msg.value> heightBid,"Please Bid more.");      
+        bids[msg.sender].isBid=true;
+        uint temp =bids[msg.sender].amount +=msg.value;       
+        if(heightBid<temp){
+            heightBid=temp;
+            heightBider=msg.sender;
+        }
+       
+    }
+    function end() public{
+        
+    }
+    
 }
