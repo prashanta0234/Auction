621bd83b388e973dffa4cea09499ae875055dbb2
"Prashanta Chakraborty" <"prashanta0234@gmail.com">
2022-05-05 06:44:19 +0600

    Complete

--- Auction.sol
+++ Auction.sol
@@ -10,20 +10,15 @@ contract Auction{
     address payable public owner;
     bool public isEnd;
     uint public numberOfBiders;
+    address payable winner;
+    bool public ownerChanged;
 
     struct Bidtype{
         uint amount;
-        bool isBid;
-        
+        bool isBid;        
     }
-    struct storeBiders{
-        uint amount;
-        address payable bider;
-        bool winner;
 
-    }
     mapping(address=>Bidtype) public bids;
-    mapping(uint=>storeBiders) public biddres;
     constructor(uint EndingTime,uint MinumumBid,address _owner){
         manager=payable(msg.sender);
         endingTime=block.timestamp+EndingTime;
@@ -40,15 +35,15 @@ contract Auction{
         require(isEnd==false);
         _;
     }
+    modifier isbider{
+        require(bids[msg.sender].isBid==true);
+        _;
+    }
     function bid() public payable running {
         require(msg.sender != manager,"Sorry manager not allowed for biding");
         require(bids[msg.sender].isBid==true||minimumBid <= msg.value,"Please Send minimum value");
         require(bids[msg.sender].amount+msg.value> heightBid,"Please Bid more.");      
-        storeBiders storage bidresInfo=biddres[numberOfBiders];
-        bidresInfo.amount+=msg.value;
-        bidresInfo.bider=payable(msg.sender);
-        
-        
+             
        uint temp =bids[msg.sender].amount +=msg.value;       
         if(heightBid<temp){
             heightBid=temp;
@@ -63,8 +58,30 @@ contract Auction{
     }
     function end() public isManger{
         isEnd=true;
-
+        winner=payable(heightBider);
 
     }
+    function changeOwner()  public isManger{
+        require(ownerChanged==false && isEnd==true,"Sorry Its not end yet.");
+        ownerChanged=true;
+         uint percent= (heightBid*10)/100;
+        owner.transfer(heightBid-percent);
+        owner=winner;
+       
+        manager.transfer(percent);
+
+    }
+
+    function Refund() public isbider payable{
+        require(ownerChanged==true && isEnd==true,"Sorry Its not allowed");
+        require(msg.sender != winner);
+        address payable requestSender= payable(msg.sender);
+        requestSender.transfer(bids[msg.sender].amount);
+
+    }
+    function balance() public view returns(uint _re){
+        _re=address(this).balance;
+    }
+
     
 }
